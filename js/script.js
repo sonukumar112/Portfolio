51e537bbb3e26b2511397ba82f979cd17c8a5c1a
Sonu Kumar sah <123002386+sonukumar112@users.noreply.github.com>
2025-06-07 20:10:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,8 +26,7 @@ window.onscroll = () => {
 
         };
     });
-
-
+    
 /* sticky navbar*/
 let header = document.querySelector('header');
 header.classList.toggle('sticky', window,scrollY > 100);
